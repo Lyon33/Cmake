c459e04076b8bec7d3ef3632bcd124831f20bef3
Lyon33 <786208769@qq.com>
2022-04-10 22:35:07 +0800

    333

---
+++ src/test3.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+* File Name: test3.cpp
+* Author: Lyon
+* Mail: 786208769@qq.com
+* Created Time: 日  4/10 22:34:06 2022
+*************************************************************************/
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world" << std::endl;
+    std::cout << "这里终于用oh my zsh 成功连接上github了!\n";
+    return 0;
+}
+
