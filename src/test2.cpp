03db44c1067a4bf1b3502789d0510a2b9ee934f4
Lyon33 <786208769@qq.com>
2022-04-10 22:09:46 +0800

    2

---
+++ src/test2.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+* File Name: test2.cpp
+* Author: Lyon
+* Mail: 786208769@qq.com
+* Created Time: 日  4/10 22:07:35 2022
+*************************************************************************/
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world" << std::endl;
+    std::cout << "Tmux 的具体使用.\n";
+
+
+    return 0;
+}
+
